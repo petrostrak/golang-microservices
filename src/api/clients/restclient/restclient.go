6b25a05597bcd10a4157cceeb5d78c6b9570687b
petrostrak <pit.trak@gmail.com>
2021-02-16 22:20:34 +0200

    test with mockup

--- src/api/clients/restclient/restclient.go
+++ src/api/clients/restclient/restclient.go
@@ -3,10 +3,49 @@ package restclient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
+var (
+	enabledMocks = false
+	mocks        = make(map[string]*Mock)
+)
+
+type Mock struct {
+	URL        string
+	HTTPMethod string
+	Response   *http.Response
+	Err        error
+}
+
+func getMockId(httpMethod string, url string) string {
+	return fmt.Sprintf("%s %s", httpMethod, url)
+}
+
+func StartMockups() {
+	enabledMocks = true
+}
+
+func StopMockups() {
+	enabledMocks = false
+}
+
+func AddMockup(mock Mock) {
+	mocks[getMockId(mock.HTTPMethod, mock.URL)] = &mock
+}
+
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	if enabledMocks {
+		mock := mocks[getMockId(http.MethodPost, url)]
+		if mock == nil {
+			return nil, errors.New("no mockup for given response")
+		}
+		return mock.Response, mock.Err
+		// return local mock without calling any external resource
+	}
+
 	JSONBytes, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
