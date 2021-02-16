432177669cefb7273ddef1cfc0ecfbcf3384c059
petrostrak <pit.trak@gmail.com>
2021-02-16 22:20:11 +0200

    testing with mockups

--- src/api/providers/github_provider/github_provider_test.go
+++ src/api/providers/github_provider/github_provider_test.go
@@ -1,7 +1,11 @@
 package github_provider
 
 import (
+	"errors"
 	"fmt"
+	"golang-microservices/src/api/clients/restclient"
+	"golang-microservices/src/api/domain/github"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +22,17 @@ func TestDefer(t *testing.T) {
 	defer fmt.Println("3")
 
 	fmt.Println("function's body")
+}
+
+func TestCreateRepoErrorRestclient(t *testing.T) {
+	restclient.StartMockups()
+	restclient.AddMockup(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Err:        errors.New("invalid restclient response"),
+	})
+
+	resp, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, resp)
+	assert.NotNil(t, err)
 }
